e401ca4d9e2d821ebed869ba2909c3ae6580ec46
sugizaki <esuyugiai@gmail.com>
2021-08-14 20:11:00 +0900

    DFSとDPについて

---
+++ DFS_DP_クリティカルパス.py
@@ -0,0 +1,27 @@
+n, m = map(int, input().split())
+
+G = [[] for _ in range(n)]
+dp = [-1] * n
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    G[a].append((b, c))
+
+print(G)
+
+
+def rec(v):
+    if dp[v] > -1:
+        return dp[v]
+
+    cost = 0
+    for nv in G[v]:
+        cost = max(cost, rec(nv[0]) + nv[1])
+
+    dp[v] = cost
+
+    return cost
+
+
+rec(0)
+print(dp[0])
