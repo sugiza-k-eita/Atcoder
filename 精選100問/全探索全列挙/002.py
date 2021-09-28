9f9bcf2349aa1504772a3b3a0d5256e8943eb114
DESKTOP-QQ4QPCF\sugiz <esuyugiai@gmail.com>
2021-09-28 21:32:41 +0900

    精選100問　001-002

---
+++ 精選100問/全探索全列挙/002.py
@@ -0,0 +1,12 @@
+# https://atcoder.jp/contests/abc106/tasks/abc106_b
+N = int(input())
+ans = 0
+for i in range(1, N+1):
+    cnt = 0
+    for j in range(1, i+1):
+        if i % 2 != 0:
+            if i % j == 0:
+                cnt += 1
+    if cnt == 8:
+        ans += 1
+print(ans)
