9256e66cd6cd80ec2452412bc6f2cc322b7a8ee8
Sugizaki Keita <esuyugiai@gmail.com>
2021-12-17 13:09:25 +0900

    UnionFindの問題を解いた。ついでにライブラリの整理もした。

---
+++ 典型関数/優先度付きキュー.py
@@ -0,0 +1,38 @@
+# python標準ライブラリのheapqは大変使いづらいので、使いやすいようクラスにしておくと良いです
+# なお、このクラスにバグがあっても責任は一切取りません、自分で書いてね
+# https://qiita.com/u2dayo/items/6debaf81fd39f82a4280#d%E5%95%8F%E9%A1%8Cquerying-multiset-
+# https://atcoder.jp/contests/abc212/tasks/abc212_d
+
+class PriorityQueue:
+    def __init__(self, a=None):
+        import heapq
+        self.heapq = heapq
+        self.__container = []
+        if a:
+            self.__container = a[:]
+        self.heapq.heapify(self.__container)
+
+    @property
+    def is_empty(self):
+        return not self.__container
+
+    def pop(self):
+        #最小値を取り出す
+        return self.heapq.heappop(self.__container)
+
+    def push(self, x):
+        #xをheapにpush
+        self.heapq.heappush(self.__container, x)
+
+    def sum(self):
+        #合計値を出力
+        return sum(self.__container)
+
+    def __len__(self):
+        return len(self.__container)
+
+    def __str__(self):
+        return str(sorted(self.__container))
+
+    def __repr__(self):
+        return self.__str__()
